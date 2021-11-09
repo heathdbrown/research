349981aa223c59cbb77f5306dbc5b4b94397563a
Heath Brown <heathd.brown@users.noreply.github.com>
2021-11-09 16:39:16 -0600

    More stuff I had to modify but it is still broken

--- awx-in-docker/install.sh
+++ awx-in-docker/install.sh
@@ -10,11 +10,16 @@ function install_awx() {
 	wget https://github.com/ansible/awx/archive/refs/tags/${AWX_VERSION}.tar.gz -q -O- - | tar xzv 
 	cd awx-${AWX_VERSION}/
 	# podman instead of docker
-	sed -i 's/docker /podman /g' Makefile
+	#sed -i 's/docker /podman /g' Makefile
 	# Same adjustment here for podman-compose
-	se -i 's/docker-compose -f/podman-compose -f/g' Makefile
-    make docker-compose-build
-    make docker-compose
+	#sed -i 's/docker-compose -f/podman-compose -f/g' Makefile
+	# Remove docker-auth to fix issues
+	sed -i 's/docker-auth //g' Makefile
+	sed -i 's/docker-compose-sources: .git\/hooks\/pre-commit/docker-compose-sources:/g' Makefile
+	sed -i "s/docker info | grep 'Operating System'/\"docker info | grep '    distribution:' | cut -d ':' -f 2 \"/g" tools/docker-compose/ansible/roles/sources/tasks/main.yml
+	#sed -i "s/docker info | grep 'Operating System'/\"docker info | grep '    distribution:' | awk '{print \$2}'\"/g" tools/docker-compose/ansible/roles/sources/tasks/main.yml
+        make docker-compose-build
+        make docker-compose
 }
 
 function main() {
